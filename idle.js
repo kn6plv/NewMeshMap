0147fa66377deca37372fcac84b005df4f01b24c
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2024-03-19 10:18:55 -0700

    Reset after idle

--- idle.js
+++ idle.js
@@ -1,16 +1,15 @@
 function idle() {
     let idle = null;
     const patrol = {
+        idling: false,
         next: 0,
-        steps: config.patrol || [ "standard", `${config.lat}/${config.lon}/${config.zoom}`, `wait ${config.idle}` ],
-        moving: false
+        steps: config.patrol || [ "standard", `${config.zoom}/${config.lat}/${config.lon}`, `wait ${config.idle}` ]
     };
     function patrolStep() {
         idle = null;
         for (;;) {
             const step = patrol.steps[patrol.next];
             patrol.next = (patrol.next + 1) % patrol.steps.length;
-            patrol.moving = false;
             if (step.indexOf("wait") === 0) {
                 const wait = parseFloat(step.substring(4)) || 30;
                 idle = setTimeout(patrolStep, wait * 1000);
@@ -20,7 +19,6 @@ function idle() {
                 let rotTime = parseFloat(step.substring(6)) || 30;
                 const halfRotTime = 30;
                 function rot() {
-                    patrol.moving = false;
                     if (idle) {
                         return;
                     }
@@ -29,7 +27,6 @@ function idle() {
                         rotTime -= time;
                         map.rotateTo((map.getBearing() + 179.9 / halfRotTime * time) % 360, { duration: time * 1000, easing: t => t });
                         map.once("moveend", rot);
-                        patrol.moving = true;
                     }
                     else {
                         patrolStep();
@@ -57,14 +54,12 @@ function idle() {
                 }
                 if (loc.length == 5) {
                     openPopup();
-                    patrol.moving = false;
                     map.flyTo({ center: [ parseFloat(loc[2]), parseFloat(loc[1]) ], speed: 1, zoom: parseFloat(loc[0]), pitch: parseFloat(loc[4]), bearing: parseFloat(loc[3]), padding: { top: 400, right: 400, bottom: 0, left: 0 } });
                     map.once("moveend", () => {
                         if (!idle) {
                             patrolStep();
                         }
                     });
-                    patrol.moving = true;
                     break;
                 }
             }
@@ -74,14 +69,17 @@ function idle() {
         patrol.next = 0;
         clearTimeout(idle);
         idle = setTimeout(function() {
+            patrol.idling = true;
             openPopup();
             filterKey();
             setMode("normal");
             patrolStep();
         }, config.idle * 1000);
-        if (patrol.moving) {
-            patrol.moving = false;
-            map.rotateTo(map.getBearing(), { duration: 0 });
+        if (patrol.idling) {
+            patrol.idling = false;
+            openPopup();
+            selectMap("standard");
+            map.flyTo({ center: [ config.lon, config.lat ], speed: 1, zoom: config.zoom, pitch: 0, bearing: 0 });
         }
     }
     [ "mousemove", "mousedown", "touchstart", "click", "keypress", "scroll" ].forEach(function(name) {
